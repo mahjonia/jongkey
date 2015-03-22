40c040ffe8167faa77a337761a59ec5f89e461c9
msuz <suzukimasahiko.jp@gmail.com>
2015-03-22 14:18:50 +0900

    add new target custom keyboard.

---
+++ custom_keyboard/KeyboardViewController.h
@@ -0,0 +1,13 @@
+//
+//  KeyboardViewController.h
+//  custom_keyboard
+//
+//  Created by スズキ マサヒコ on 2015/03/22.
+//  Copyright (c) 2015年 mahjonia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KeyboardViewController : UIInputViewController
+
+@end
